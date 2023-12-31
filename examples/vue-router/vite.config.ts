dad1b09e59022602e1292c8d7a33f56634ed4f26
userquin <userquin@gmail.com>
2023-12-31 16:20:03 +0100

    chore: enable example logs when running dev build

--- examples/vue-router/vite.config.ts
+++ examples/vue-router/vite.config.ts
@@ -5,13 +5,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   mode: process.env.MODE === 'production' ? 'production' : 'development',
+  build: {
+    minify: process.env.MODE === 'production',
+  },
   define: {
-    __SW__: '"/sw.js"',
-    __SW_SCOPE__: '"/"',
-    __SW_TYPE__: '"classic"',
-    __DATE__: `${JSON.stringify(Date.now())}`,
-    AUTO_SW: `${JSON.stringify(process.env.SW_AUTO === 'true')}`,
-    AUTO_DESTROY_SW: 'false',
+    '__SW__': '"/sw.js"',
+    '__SW_SCOPE__': '"/"',
+    '__SW_TYPE__': '"classic"',
+    '__DATE__': `${JSON.stringify(Date.now())}`,
+    'AUTO_SW': `${JSON.stringify(process.env.SW_AUTO === 'true')}`,
+    'AUTO_DESTROY_SW': 'false',
+    'process.env.NODE_ENV': `"${process.env.MODE ?? 'production'}"`,
   },
   plugins: [vue()],
 })
